3a62b777c9c9a340fcb780ba8a30e0a0aa800296
Bigizic <olalekanisaac75@gmail.com>
2024-01-02 12:30:09 +0000

    add files

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""BasicCache class that inherits from BaseCaching and is a caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """A BasicCache class inheriting from BaseCaching and
+    proceeds to define certain methods
+    """
+
+    def __init__(self):
+        """Constructor
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """Assigns to the dictionary from BaseCaching the item value of the
+        key
+        """
+        if key or item in [None]:
+            pass
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrives the value of a key from self.cache_data
+        """
+        if key:
+            for k, v in self.cache_data.items():
+                if k == key:
+                    return v
+        return None
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A class FIFOCache that inherits from BaseCaching and is a caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
