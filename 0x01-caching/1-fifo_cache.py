e6a950bee3ea39adac4a6bb79ee817bc8bf8e3f6
Bigizic <olalekanisaac75@gmail.com>
2024-01-02 12:52:15 +0000

    add files

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -6,4 +6,32 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """A FIFO caching system
     """
+
+    def __init__(self):
+        """Constructor
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """Assigns to the dictionary from BaseCaching the 'item' value of the
+        key 'key'
+        """
+        if key or item in [None]:
+            pass
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_item = [x for x, v in self.cache_data.items()]
+            self.cache_data.pop(first_item[0])
+            print(f"DISCARD: {first_item[0]}")
+
+    def get(self, key):
+        """Retrives the value of a key from self.cache_data
+        """
+        if key:
+            for k, v in self.cache_data.items():
+                if k == key:
+                    return v
+        return None
