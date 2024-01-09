71ed83d3b6b0bf4b890b16fde8fc7a9e3e4d0c48
Bigizic <olalekanisaac75@gmail.com>
2024-01-09 20:35:11 +0000

    add new files

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""A flask route that uses flask Babel to configure en to fr
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+class Config():
+    """Implementation of the config class that has a language class
+    attribute equal to ["en", "fr"]
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+babel = Babel(app)
