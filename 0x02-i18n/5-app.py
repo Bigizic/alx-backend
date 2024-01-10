85e191197e3834fd9416e8a08d39542b8e76ce9e
Bigizic <olalekanisaac75@gmail.com>
2024-01-10 12:40:49 +0000

    update 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -47,7 +47,7 @@ def get_user() -> Union[Dict, None]:
         return users.get(int(u_id))
     return None
 
-
+@babel.localeselector
 def get_locale() -> str:
     """Retreives the locale for a web page from request
     """
@@ -56,9 +56,6 @@ def get_locale() -> str:
             return request.args.get('locale')
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
