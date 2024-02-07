937c2677be7d1d4b19cc7fd71b304d492f461523
Bigizic <olalekanisaac75@gmail.com>
2024-02-07 00:07:28 +0000

    add task 5

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+    return;
+});
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+const data = {
+    'Portland': 50,
+    'Seattle': 80,
+    'New York': 20,
+    'Bogota': 20,
+    'Cali': 40,
+    'Paris': 2
+};
+
+for (const keys in data) {
+    client.HSET('HolbertonSchools', keys, data[keys], (err, reply) => {
+        if (err) { throw(err); } else { console.log(reply); };
+    });
+}
+client.HGETALL('HolbertonSchools', (err, reply) => {
+    if (err) { throw(err); } else { console.log(reply); };
+})
