920a3e21832f5fc2563e66eeb5049373a5f4fca1
Bigizic <olalekanisaac75@gmail.com>
2024-02-07 00:57:53 +0000

    add task 7 and 8

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+const jobData = {
+    'phoneNumber': '23412345678',
+    'message': 'Hello Daniel'
+};
+
+const job = queue.create('push_notification_code', jobData)
+  .save(function(err) {
+    if (!err) {
+      console.log('Notification job created:', job.id);
+    }
+});
+
+job.on('complete', function() {
+  console.log('Notification job completed');
+});
+
+job.on('failed', function() {
+  console.log('Notification job failed');
+});
